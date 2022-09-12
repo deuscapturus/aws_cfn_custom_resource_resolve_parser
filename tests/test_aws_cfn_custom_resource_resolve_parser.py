d86c3bd58fb5f5281501056f2d50549a0ae1e0fe
Theodore Cowan <theodore-cowan@pluralsight.com>
2022-09-12 14:00:11 -0400

    feat: support secrets manager dynamic resource in more ways

--- tests/test_aws_cfn_custom_resource_resolve_parser.py
+++ tests/test_aws_cfn_custom_resource_resolve_parser.py
@@ -22,6 +22,53 @@ def test_parsing_arn():
     )
     assert secret_value[1] == "BOOTSTRAP_SERVERS"
 
+def test_parsing_arn_with_key_missing_versions():
+    secret = (
+        r"{{resolve:secretsmanager:arn:aws:secretsmanager:eu-west-1:012345678912:secret:kafka/eu-west-1/server01"
+        r":SecretString:BOOTSTRAP_SERVERS::}}"
+    )
+    secret_value = parse_secret_resolve_string(secret)
+    assert (
+        secret_value[0]
+        == "arn:aws:secretsmanager:eu-west-1:012345678912:secret:kafka/eu-west-1/server01"
+    )
+    assert secret_value[1] == "BOOTSTRAP_SERVERS"
+
+def test_parsing_arn_with_only_version_id():
+    secret = (
+        r"{{resolve:secretsmanager:arn:aws:secretsmanager:eu-west-1:012345678912:secret:kafka/eu-west-1/server01"
+        r":SecretString:::7b4bdaaa-32b5-11ed-8bdc-00051bcc25e0}}"
+    )
+    secret_value = parse_secret_resolve_string(secret)
+    assert (
+        secret_value[0]
+        == "arn:aws:secretsmanager:eu-west-1:012345678912:secret:kafka/eu-west-1/server01"
+    )
+    assert secret_value[3] == "7b4bdaaa-32b5-11ed-8bdc-00051bcc25e0"
+
+def test_parsing_arn_with_missing_version_and_key():
+    secret = (
+        r"{{resolve:secretsmanager:arn:aws:secretsmanager:eu-west-1:012345678912:secret:kafka/eu-west-1/server01"
+        r":SecretString:::}}"
+    )
+    secret_value = parse_secret_resolve_string(secret)
+    assert (
+        secret_value[0]
+        == "arn:aws:secretsmanager:eu-west-1:012345678912:secret:kafka/eu-west-1/server01"
+    )
+
+def test_parsing_arn_with_key_and_version_stage():
+    secret = (
+        r"{{resolve:secretsmanager:arn:aws:secretsmanager:eu-west-1:012345678912:secret:kafka/eu-west-1/server01"
+        r":SecretString:BOOTSTRAP_SERVERS:AWSCURRENT:}}"
+    )
+    secret_value = parse_secret_resolve_string(secret)
+    assert (
+        secret_value[0]
+        == "arn:aws:secretsmanager:eu-west-1:012345678912:secret:kafka/eu-west-1/server01"
+    )
+    assert secret_value[1] == "BOOTSTRAP_SERVERS"
+    assert secret_value[2] == "AWSCURRENT"
 
 def test_parsing_name():
     secret = r"{{resolve:secretsmanager:/kafka/eu-west-1/server01:SecretString:BOOTSTRAP_SERVERS::AWSPENDING}}"
